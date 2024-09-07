3264c627a2bdb31ab63df4f31306ab121b4c2568
Klinton Chakma <159724195+klinton404@users.noreply.github.com>
2024-09-07 22:54:45 +0600

    Add files via upload

---
+++ How to know database version level and edition.sql
@@ -0,0 +1,5 @@
+--To know Database version, level and edition
+
+Select Serverproperty ('productversion') as Database_version,
+Serverproperty ('productlevel') as Database_level,
+Serverproperty ('edition') as Database_Edition
